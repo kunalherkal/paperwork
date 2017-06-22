0c43b15607d5a25f8cd9a2c5fcc203e583699274
Kunal Herkal <kunalrherkal@gmail.com>
2017-06-22 16:48:12 +0530

    Added views.

--- src/main/java/com/kunalherkal/paperwork/controllers/PersonController.java
+++ src/main/java/com/kunalherkal/paperwork/controllers/PersonController.java
@@ -3,6 +3,8 @@ package com.kunalherkal.paperwork.controllers;
 import com.kunalherkal.paperwork.models.dto.Person;
 import com.kunalherkal.paperwork.services.DocumentCreator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 
-@RestController
+@Controller
 public class PersonController {
     private DocumentCreator documentCreator;
 
@@ -25,5 +27,11 @@ public class PersonController {
         Person person = new Person(name, "Herkal", 25);
         documentCreator.create(person);
         return person;
+    }
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value = "name", defaultValue = "") String name, Model model) throws IOException {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
