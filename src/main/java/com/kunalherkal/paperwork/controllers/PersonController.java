101d79875f16603a99995cd2b80a00f68710e4de
Kunal Herkal <kunalrherkal@gmail.com>
2017-06-23 13:59:34 +0530

    download document basic api working.

--- src/main/java/com/kunalherkal/paperwork/controllers/PersonController.java
+++ src/main/java/com/kunalherkal/paperwork/controllers/PersonController.java
@@ -1,16 +1,22 @@
 package com.kunalherkal.paperwork.controllers;
 
-import com.kunalherkal.paperwork.models.dto.Person;
 import com.kunalherkal.paperwork.services.DocumentCreator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 @Controller
@@ -22,16 +28,28 @@ public class PersonController {
         this.documentCreator = documentCreator;
     }
 
-    @RequestMapping(value = "/person", method = RequestMethod.GET)
-    public Person getPerson(@RequestParam(value = "name", defaultValue = "") String name) throws IOException {
-        Person person = new Person(name, "Herkal", 25);
-        documentCreator.create(person);
-        return person;
-    }
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String greeting(@RequestParam(value = "name", defaultValue = "") String name, Model model) throws IOException {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public ResponseEntity<ByteArrayResource> download(@RequestParam(value = "name", defaultValue = "") String name) throws IOException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        File file = new File("document1.docx");
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource byteArrayResource = new ByteArrayResource(Files.readAllBytes(path));
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(byteArrayResource);
     }
 }
